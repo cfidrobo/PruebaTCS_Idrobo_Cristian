8df75ea30c0eadd245448b20366b7bcc03f110e5
cfidrobo <cfidrobo@espe.edu.ec>
2025-07-13 05:35:51 -0500

    build (ci): Pruebas en ci.yml despliegue terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,6 @@
 provider "google" {
-  project     = var.project
-  region      = var.region
-  credentials = var.credentials
+  project = var.project
+  region  = var.region
 }
 
 data "google_client_config" "current" {}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,5 @@
 variable "project"    { type = string }
 variable "region"     { type = string }
-variable "credentials" { type = string }
 variable "image" {
   description = "Container image to deploy"
   type        = string
